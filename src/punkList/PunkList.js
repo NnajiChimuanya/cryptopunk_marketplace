463d435ad3a3628f489ef17320af78017fe8b2ba
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-07-04 08:16:55 +0400

    debug

--- src/punkList/PunkList.js
+++ src/punkList/PunkList.js
@@ -14,7 +14,7 @@ const PunkList = () => {
         "https://testnets-api.opensea.io/assets?asset_contract_address=0xE3320a32f3c5959845C6A39b3B0d29D869A8F968&order_direction=asc"
       );
       setPunkListData(data.data.assets);
-      console.log(data.data.assets))
+      console.log(data.data.assets);
     };
 
     getNfts();
