b35719471086e85e09a497b5f0962a9367614b91
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-07-06 00:00:07 +0400

    Made a little bug fix

--- src/punkList/PunkList.js
+++ src/punkList/PunkList.js
@@ -15,7 +15,7 @@ const PunkList = () => {
       let data = await axios.get(
         "https://cryptopunk-marketplace-backend.herokuapp.com/getNfts"
       );
-      setPunkListData(data.data);
+      setPunkListData(data.data.reverse());
       console.log(data.data);
     };
 
@@ -36,19 +36,23 @@ const PunkList = () => {
     <div>
       {punkListData.length > 0 && (
         <div className="main">
-          <div className="punk-highligt">
+          <div className="punk-highlight">
             <img src={activePunk?.image_original_url} alt="" />
+          </div>
+          <div className="details">
+            <div className="details-punk"></div>
+            <div className=""></div>
           </div>
         </div>
       )}
 
       <div unselectable="on" className="punk-list">
-        {punkListData.map((punk) => {
-          const { id, token_id, image_original_url, name, traits } = punk;
+        {punkListData.map((punk, id) => {
+          const { token_id, image_original_url, name, traits } = punk;
           return (
             <div key={id} onClick={() => handleClick(token_id)}>
               <CollectionCard
-                onClick={handleClick}
+                onClick={() => handleClick(id)}
                 key={id}
                 image={image_original_url}
                 price={traits[0].value}
